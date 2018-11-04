45b963792690abc886dbbb959bf9050223eff2ae
Your Name <you@example.com>
2018-11-04 14:52:33 +0000

    first commit

---
+++ src/main/java/com/bahawi/GestionStockBackEndV2Application.java
@@ -0,0 +1,12 @@
+package com.bahawi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockBackEndV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockBackEndV2Application.class, args);
+	}
+}
